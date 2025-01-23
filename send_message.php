2974e55b0b8128dc623cf88490bb6f5e17d806ef
Giramon <120495791+Giramon@users.noreply.github.com>
2025-01-23 23:49:15 +0500

    send

--- send_message.php
+++ send_message.php
@@ -1,40 +1,39 @@
 <?php
 
+// All
+$message = "Привет! Это автопостинг сообщения.";
+$imagePath = "img/UtcNPjnOTDA.jpg";
+
+// TG
 $botToken = "7916625696:AAEW0Pi5t4X0hBCRD-x4S6AQ8hgwxPP188o";
 $chatId = "-1002255910020";
 
-// Сообщение, которое вы хотите отправить
-$message = "Привет! Это автопостинг сообщения.";
+// TG
+function sendPhotoWithCaption($chatId, $photoPath, $caption, $botToken) {
+    $url = "https://api.telegram.org/bot$botToken/sendPhoto";
 
-// Функция для отправки сообщения в Telegram
-function sendMessage($chatId, $message, $botToken) {
-    $url = "https://api.telegram.org/bot$botToken/sendMessage";
-
-    $data = [
+    // Используем CURL для отправки файла
+    $postFields = [
         'chat_id' => $chatId,
-        'text' => $message,
+        'photo' => new CURLFile(realpath($photoPath)),
+        'caption' => $caption,
         'parse_mode' => 'HTML'
     ];
 
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+    
     $response = curl_exec($ch);
     curl_close($ch);
 
     return $response;
 }
 
-// Отправляем сообщение
-$response = sendMessage($chatId, $message, $botToken);
-
-// Выводим ответ от Telegram API (для отладки)
-echo "Response: " . $response;
+// Отправляем изображение с текстом
+$responsePhoto = sendPhotoWithCaption($chatId, $imagePath, $message, $botToken);
+echo "Response from sendPhotoWithCaption: " . $responsePhoto . "\n";
 
 ?>
-
-//$url = "https://api.telegram.org/bot$botToken/getUpdates";
-   $response = file_get_contents($url);
-   echo $response;
